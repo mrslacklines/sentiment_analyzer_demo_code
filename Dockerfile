1766754b89f2c40433f00b8ff4b22603b8c25a36
Tomasz Kuczmarski <tomasz.kuczmarski@stxnext.pl>
2015-10-16 16:40:17 +0200

    Adding supervisor conf, moving twitter aggregator to a script

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,13 @@
 FROM ubuntu:latest
 MAINTAINER CityGlobe
 
+RUN mkdir -p /opt/mbsaa/aggregator
+COPY ./ /opt/mbsaa/aggregator
+
 RUN apt-get -y update
 RUN DEBIAN_FRONTEND=noninteractive apt-get -y install -y python2.7 python-pip python-dev build-essential libffi-dev libssl-dev libxml2-dev libxslt1-dev
 
 RUN pip install --upgrade pip
-
-COPY ./ .
-
-RUN pip install -r requirements.txt
+RUN pip install --upgrade requests[security]
+RUN pip install supervisor --pre
+RUN pip install -r /opt/mbsaa/aggregator/requirements.txt
---
+++ scripts/listen_to_twitter_stream.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+
+"""
+Twitter stream listener
+Aggregates tweets for a given set of keywords
+"""
+
+from __future__ import unicode_literals
+
+import argparse
+import sys
+from flatdict import FlatDict
+from tweepy import (
+    API, OAuthHandler, Stream, StreamListener, TweepError)
+from yaml import safe_load
+
+
+class Aggregator(StreamListener):
+
+    counter = 0
+    api = None
+    configuration = None
+
+    def __init__(self, config_file_handle):
+        self.configuration = self._read_config(config_file_handle)
+        self.counter = 0
+        self.api = self.api or self._set_twitter_rest_api()
+
+    def on_error(self, status_code):
+        print status_code
+
+    def on_disconnect(self):
+        self.counter = 0
+
+    def on_status(self, status):
+        self.counter += 1
+        print str(self.counter) + ' ' + status.text
+
+    def _read_config(self, filehandle):
+        return safe_load(filehandle)
+
+    def _set_oauth(self):
+        """
+        Sets Twitter OAuth for authenticated API calls
+        """
+        auth = OAuthHandler(
+            self.configuration.get('OAUTH', {}).get('CONSUMER_KEY'),
+            self.configuration.get('OAUTH', {}).get('CONSUMER_SECRET'))
+        auth.set_access_token(
+            self.configuration.get('OAUTH', {}).get('ACCESS_TOKEN'),
+            self.configuration.get('OAUTH', {}).get('ACCESS_TOKEN_SECRET'))
+        return auth
+
+    def _set_twitter_rest_api(self):
+        """
+        Sets Twitter authenticated REST API object
+        """
+        api = API(self._set_oauth())
+        return api
+
+    def _set_twitter_stream_api(self):
+        """
+        Sets Twitter authenticated Stream API object
+        """
+        auth = self._set_oauth()
+        stream = Stream(auth, self)
+        return stream
+
+    def get_twitter_posts_by_rest(self, keyword, since=None, limit=125):
+        """
+        Gets Twitter posts for a given keyword
+        """
+        tweets = []
+        max_id = -1L
+        count = 0
+        filter_params = ['#' + hashtag + ' ' + '#' + keyword for hashtag
+                         in self.configuration.get(
+                             'TWITTER', {}).get('HASHTAGS')]
+        query = " OR ".join(filter_params)
+
+        while count < self.configuration.get('TWITTER', {}).get('MAX_TWEETS'):
+            try:
+                if (max_id <= 0):
+                    new_tweets = self.api.search(
+                        q=query, count=self.configuration.get(
+                            'OAUTH', {}).get('MAX_QUERY'), lang="en",
+                        since_id=since)
+                else:
+                    new_tweets = self.api.search(
+                        q=query, count=self.configuration.get(
+                            'TWITTER', {}).get('MAX_QUERY'), lang="en",
+                        since_id=since, max_id=str(max_id - 1))
+                if not new_tweets:
+                    print("No new tweets")
+                    break
+                tweets.extend(new_tweets)
+                count += len(new_tweets)
+                print("Downloaded {0} tweets".format(count))
+                max_id = new_tweets[-1].id
+            except TweepError as e:
+                print(e)
+                break
+        return tweets
+
+    def _check_search_request_limits(self, api):
+        """
+        Checks remaining Twitter rate limit time
+        """
+        limits = api.rate_limit_status()
+        flat_limits = FlatDict(limits)
+        timestamp = flat_limits.get('resources:search:/search/tweets:reset')
+        return timestamp
+
+    def get_twitter_posts_by_stream(self, keyword):
+        """
+        Collects tweets for a given set of keywords
+        """
+        stream = self._set_twitter_stream_api()
+        filter_params = ['#' + hashtag + ' ' + keyword for hashtag
+                         in self.configuration.get(
+                             'TWITTER', {}).get('HASHTAGS')]
+        # print filter_params
+        stream.filter(
+            track=filter_params, async=True, languages=self.configuration.get(
+                'OAUTH', {}).get('LANGUAGES'))
+        return stream
+
+
+def main(arguments):
+
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        'config', help="Config file", type=argparse.FileType('r'))
+
+    args = parser.parse_args(arguments)
+    while True:
+        try:
+            aggregator = Aggregator(args.config)
+            aggregator.get_twitter_posts_by_stream('boston')
+
+        except Exception, e:
+            print "Error. Restarting Stream.... Error: "
+            print e.__doc__
+            print e.message
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
